e6fab2a056c9c8a0632f6c6cf353c5fd6830fc65
TaisGuardamino <a20212192@pucp.edu.pe>
2024-06-30 22:25:44 -0500

    Update mapafinal.py

--- mapafinal.py
+++ mapafinal.py
@@ -4,28 +4,30 @@ import json
 import streamlit as st
 from streamlit_folium import folium_static
 
-# Especifica las rutas de tus archivos
+# Especificamos las rutas de nuestros archivos
+## Base de datos de casos de dengue por departamentos:
 ruta_csv = 'dengue.csv'
+## Archivo Json del mapa de Perú dividido por epartamentos: 
 ruta_geojson = 'peru_departamental_simple.geojson'
 
-# Lee el archivo CSV en un DataFrame
+# Leemos el archivo CSV en un DataFrame
 df = pd.read_csv(ruta_csv)
 
-# Normaliza los nombres de las regiones en el DataFrame
+# Normalizamos los nombres de las regiones en el DataFrame
 df['Departamentos'] = df['Departamentos'].str.strip().str.upper()
 
-# Verifica las primeras filas del DataFrame
+# Verificamos las primeras filas del DataFrame
 print(df.head())
 
-# Lee el archivo GeoJSON
+# Leemos el archivo GeoJSON
 with open(ruta_geojson) as f:
     geojson_data = json.load(f)
 
-# Función para crear el mapa
+# Creamos el mapa
 def crear_mapa(año):
     mapa = folium.Map(location=[-9.19, -75.0152], zoom_start=5)
 
-    # Crear una función de estilo para el mapa de densidad
+    # Creamos una función de estilo para el mapa de densidad (Asignamos colores según la cantidad de casos)
     def estilo(feature):
         Departamentos = feature['properties']['NOMBDEP']
         Departamentos = Departamentos.strip().upper()
@@ -48,14 +50,14 @@ def crear_mapa(año):
                 'fillOpacity': 0.1,
             }
 
-    # Añadir las regiones al mapa
+    # Añadimos las regiones al mapa
     folium.GeoJson(
         geojson_data,
         style_function=estilo,
         tooltip=folium.GeoJsonTooltip(fields=['NOMBDEP'], aliases=['Departamentos:'])
     ).add_to(mapa)
 
-    # Crear la leyenda como un DivIcon
+    # Creamos la leyenda como un DivIcon (Los colores y textos que indican el número de casos de dengue por departamento)
     html_leyenda = """
     <div style="position: fixed;
                 bottom: 50px; left: 50px; width: 220px; height: 200px;
@@ -73,7 +75,7 @@ def crear_mapa(año):
 """
 
     leyenda = folium.Marker(
-        location=[-12.0464, -77.0428],  # Ajusta esta ubicación si es necesario
+        location=[-12.0464, -77.0428], 
         icon=folium.DivIcon(
             icon_size=(700, 250),
             icon_anchor=(0, 0),
@@ -91,10 +93,10 @@ st.title('Mapa de Densidad Poblacional de los casos de dengue en Perú (2019 - 2
 # Subtítulo que advierte sobre la fecha de corte en los datos del año 2024
 st.subheader('Importante: El año 2024 cuenta con datos registrados hasta la semana epidemiológica 10.')
 
-# Entrada del usuario para seleccionar el año
+# Indicación para que el usuario seleccione el año
 año = st.selectbox('Seleccione el año:', options=[2019, 2020, 2021, 2022, 2023, 2024])
 
-# Crear y mostrar el mapa
+# Creamos y mostramos el mapa
 if año:
     mapa = crear_mapa(año)
     folium_static(mapa)
